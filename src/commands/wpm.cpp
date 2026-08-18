62715947b28688387fb02e65c71fdeb48a6576a0
caomengxuan666 <2507560089@qq.com>
2026-08-18 10:12:26 +0800

    release: version 0.16.1

--- src/commands/wpm.cpp
+++ src/commands/wpm.cpp
@@ -70,8 +70,8 @@ constexpr std::string_view kBuiltinIndex = R"json(
 {
   "schema": 1,
   "name": "official",
-  "version": "builtin-2026.08.14",
-  "updated": "2026-08-14",
+  "version": "builtin-2026.08.18",
+  "updated": "2026-08-18",
   "sources": [
     {
       "name": "official-github-raw",
